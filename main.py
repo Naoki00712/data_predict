255acfa8c34377221962e1f406d1017ae971d22a
Naoki00712 <kd1299709@st.kobedenshi.ac.jp>
2022-10-21 10:36:24 +0900

    add DeviationValue

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-from ast import If
 import pandas as pd
 from pycaret.classification import *
 
@@ -11,25 +10,23 @@ def score(self):
         return str("none")
 
 model = load_model('/Users/naokimatsumoto/Desktop/pyc/2021_lr')
-data_predict = pd.read_csv('/Users/naokimatsumoto/Desktop/pyc/data_predict_202207050211.csv')
+race_data = pd.read_csv('/Users/naokimatsumoto/Desktop/pyc/data_predict_202207050211.csv',index_col=0)
 
-result = predict_model(model, data = data_predict)
+result = predict_model(model, data = race_data)
 
-result_d = result.loc[:, ['horse_number', 'prediction_label', 'prediction_score']].sort_values('horse_number').reset_index(drop=True)
+result['score'] = result.apply(score,axis=1)
+score_std = result['score'].std(ddof=0)
+score_mean = result['score'].mean()
+result['DeviationValue'] = result['score'].map(lambda x: round((x - score_mean) / score_std * 10 + 50, 2))
 
-result_d['score'] = result_d.apply(score,axis=1)
+# result_d = result.loc[:, ['horse_number', 'prediction_label', 'prediction_score']].reset_index(drop=True)
 
-# data_predict_d = data_predict.drop(columns = 'Unnamed: 0')
+merge = pd.merge(race_data, result['DeviationValue'], right_index=True, left_index=True)
 
-# merge = pd.merge(result_d, data_predict_d, right_index=True, left_index=True)
+merge.to_csv('/Users/naokimatsumoto/Desktop/pyc/prediction.csv')
+
+print(merge)
 
 # path = '/Users/naokimatsumoto/Desktop/pyc/prediction.json'
 # merge.to_json(path)
-
-# print(merge)
-
-result_d.to_csv('/Users/naokimatsumoto/Desktop/pyc/prediction.csv')
-
-print(result_d)
-
 # print(data_predict)
